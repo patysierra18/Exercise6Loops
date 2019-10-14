4666a4904497470e3233f0d589f94957270ca605
Paty <patysierra1@gmail.com>
2019-10-14 13:05:31 -0500

    for loop exercises added. Continue and break sentences input

--- src/com/exercise6loops/app/ForLoop.java
+++ src/com/exercise6loops/app/ForLoop.java
@@ -2,9 +2,50 @@ package com.exercise6loops.app;
 
 public class ForLoop {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		//Constant declaration
+		
+		//Variable declaration
+		
+		//Objects construction
+		
+		//Create a series of 1-100
+		for(int i=0;i<=100;i++)
+		//For(int i=100;i>1;i--)
+		{
+			System.out.println(i);
+			
+		}
+		for (int i=0;i<=100;i+=2)
+		{
+				System.out.println(i);
+		}
+		
+		System.out.println("Display odd numbres from 100 to 200");
+		for (int i= 0;i<200;i++)
+		{
+			if(i%2==0)
+			{
+				continue;
+			}
+			System.out.println(i);
+		}
+	
+		System.out.println("Display just the half of the sequence 100 to 200");
 
+		for(int i =100;i<200;i++)
+		{
+			System.out.println(i);
+			if (i==150)
+			{
+				break;
+				
+			}
+		}
+	
 	}
+	
+	
 
 }
